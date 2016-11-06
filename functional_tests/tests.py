673601a953a6befdd194950ae74227f42a2aa67f
Kevis <kevinisaac11@gmail.com>
2016-11-06 12:03:46 +0300

    functional_tests added

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
 
 
 class TodoTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super(TodoTest, cls).setUpClass()
+        cls.server_url = cls.live_server_url
+
+    # @classmethod
+    # def tearDownClass(cls):
+    #     if cls.server_url == cls.live_server_url:
+    #         super(TodoTest, cls).tearDownClass()
+
+
     
     def setUp(self):
         self.browser = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver')
@@ -22,7 +39,7 @@ class TodoTest(StaticLiveServerTestCase):
         
 	
     def test_can_start_a_list_and_retrieve_it_later(self):
-    	self.browser.get(self.live_server_url)
+    	self.browser.get(self.server_url)
 
         #she notices the page title and header mentions to-do lists
         self.assertIn('To-do', self.browser.title)
@@ -67,7 +84,7 @@ class TodoTest(StaticLiveServerTestCase):
         self.browser = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver')
          # Francis visits the home page.  There is no sign of Edith's
         # list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy Os', page_text)
         self.assertNotIn('Buy Goats', page_text)
@@ -98,6 +115,22 @@ class TodoTest(StaticLiveServerTestCase):
 
         # self.fail('Finish the test!') 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super(NewVisitorTest, cls).setUpClass()
+        cls.server_url = cls.live_server_url
+
+    # @classmethod
+    # def tearDownClass(cls):
+    #     if cls.server_url == cls.live_server_url:
+    #         super(NewVisitorTest, cls).tearDownClass()
+
+    
     def setUp(self):
         self.browser = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver')
         self.browser.implicitly_wait(3)
@@ -109,7 +142,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         #User goes to homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         #she notices the input box is nicely centered
